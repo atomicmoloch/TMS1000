37a29d11b33da519df795d08c73ed10a6313690d
atomicmoloch <kwilson@reed.edu>
2024-11-27 14:56:31 -0800

    File handling

--- decompiler.rs
+++ decompiler.rs
@@ -1,11 +1,14 @@
 use std::fs::File;
+use std::io::Read;
 use std::str;
 
 
 fn decompile(filename : &'static str)
 {
     let file = File::open(filename);
-
+    let mut data = vec![];
+    let _ = file.expect("REASON").read_to_end(&mut data);
+    println!("{:?}", data);
 }
 
 fn main() {
