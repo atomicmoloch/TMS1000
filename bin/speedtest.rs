b12749b91d1f61d1d472294e99ec3f59952456b5
atomicmoloch <kwilson@reed.edu>
2025-05-01 02:55:57 -0700

    Near-final commit

--- bin/speedtest.rs
+++ bin/speedtest.rs
@@ -20,6 +20,26 @@ fn speedtest_300k(mut system : TMS1000::SYSTEM, k_inp : u8) {
     println!("{:?}", system.get_o_outputs());
 }
 
+fn speedtest_strobek_300k(mut system : TMS1000::SYSTEM) {
+    system.set_logging();
+    let start = SystemTime::now();
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(1);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(2);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(4);
+    }
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("Executed 300,000 instructions in {} milliseconds", duration.as_millis());
+    println!("{} microseconds per instruction", duration.as_micros() / 300000);
+    println!("Effective kHz: {}", 300000/duration.as_millis());
+    println!("{:?}", system.get_o_outputs());
+}
+
 fn speedtest_500k(mut system : TMS1000::SYSTEM, k_inp : u8) {
     system.set_logging();
     let start = SystemTime::now();
@@ -29,6 +49,32 @@ fn speedtest_500k(mut system : TMS1000::SYSTEM, k_inp : u8) {
     let end = SystemTime::now();
     let duration = end.duration_since(start).unwrap();
     println!("Executed 500000 instructions in {} milliseconds", duration.as_millis());
+    println!("{} microseconds per instruction", duration.as_micros() / 500000);
+    println!("Effective kHz: {}", 500000/duration.as_millis());
+    println!("{:?}", system.get_o_outputs());
+}
+
+fn speedtest_strobek_500k(mut system : TMS1000::SYSTEM) {
+    system.set_logging();
+    let start = SystemTime::now();
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(1);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(2);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(4);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(8);
+    }
+    for _ in 0..100000 {
+        system.instruction_cycle_mut(1);
+    }
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("Executed 500,000 instructions in {} milliseconds", duration.as_millis());
     println!("{} microseconds per instruction", duration.as_micros() / 500000);
     println!("Effective kHz: {}", 500000/duration.as_millis());
     println!("{:?}", system.get_o_outputs());
@@ -52,9 +98,12 @@ fn main() {
     speedtest_300k(system.clone(), 0);
     println!("K-inputs 1111, 300000 instructions (1 second @ 300 khz)");
     speedtest_300k(system.clone(), 15);
+    println!("Strobing K-inputs, 300000 instructions (1 second @ 300 khz)");
+    speedtest_strobek_300k(system.clone());
     println!("K-inputs 0, 500000 instructions (1 second @ 500 khz, max speed)");
     speedtest_500k(system.clone(), 0);
     println!("K-inputs 1111, 500000 instructions (1 second @ 500 khz)");
     speedtest_500k(system.clone(), 15);
-
+    println!("Strobing K-inputs, 500000 instructions (1 second @ 500 khz)");
+    speedtest_strobek_500k(system.clone());
 }
